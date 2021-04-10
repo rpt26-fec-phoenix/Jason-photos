71503e98f3a607e79f4e616c8aadd06898e14b91
jasonschreib <jwschreiber1@gmail.com>
2021-04-10 14:34:34 -0700

    Working on client - photos in correct arrangement

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -3,22 +3,30 @@ import styled from 'styled-components';
 
 const PrimaryPhoto = styled.img`
   width: 564px;
-  border: 2px solid #333;
+  height: 391px;
+  // border: 2px solid #333;
   border-radius: 8px;
   margin: 6px;
+  margin-left: 0px;
 `;
 
 const SmallerPhoto = styled.img`
   display: border-box;
   width: 264px;
-  height: 150px;
+  height: 190px;
   padding: 6px;
+  border-radius: 12px;
+  padding-bottom: 4px;
+  padding-top: 4px;
 `;
 
 const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 80% 20% 20%;
+  margin: auto;
   width: 50%;
-  border: 2px solid #333;
-  border-radius: 4px;
+  // border: 2px solid #333;
+  // border-radius: 4px;
   margin-bottom: 25px;
   padding-right: 10px;
   /*overflow: hidden; */
@@ -26,28 +34,38 @@ const Wrapper = styled.div`
   ${PrimaryPhoto}:hover {
     opacity: 50%;
   }
+
+  ${SmallerPhoto}: hover {
+    opacity: 50%;
+  }
 `;
+
+
 
 
 const Homepage = (props) => {
 
   return (
     <Wrapper>
-        <PrimaryPhoto src={props.photos[0].url} alt={'large photo'}></PrimaryPhoto>
-        <div>
+      <PrimaryPhoto src={props.photos[0].url} alt={'large photo'}></PrimaryPhoto>
+      <div>
+        <span>
           <div>
-            <span>
-              <SmallerPhoto src={props.photos[1].url} alt={'small photo1'}></SmallerPhoto>
-              <SmallerPhoto src={props.photos[3].url} alt={'small photo3'}></SmallerPhoto>
-            </span>
+            <SmallerPhoto src={props.photos[1].url} alt={'small photo1'}></SmallerPhoto>
           </div>
           <div>
-            <span>
-              <SmallerPhoto src={props.photos[2].url} alt={'small photo2'}></SmallerPhoto>
-              <SmallerPhoto src={props.photos[4].url} alt={'small photo4'}></SmallerPhoto>
-            </span>
+            <SmallerPhoto src={props.photos[3].url} alt={'small photo3'}></SmallerPhoto>
           </div>
-        </div>
+        </span>
+        <span>
+          <div>
+            <SmallerPhoto src={props.photos[2].url} alt={'small photo2'}></SmallerPhoto>
+          </div>
+          <div>
+            <SmallerPhoto src={props.photos[4].url} alt={'small photo4'}></SmallerPhoto>
+          </div>
+        </span>
+      </div>
     </Wrapper>
   );
 };
