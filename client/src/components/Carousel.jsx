5d2b709590b509fd63d2468f893321aa452331e3
jasonschreib <jwschreiber1@gmail.com>
2021-04-11 23:16:01 -0700

    All photos rendering in modal - working on display

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
 
+//import share icon
+
+//import save icon
+import SaveIcon from './Saveicon.jsx';
+
+
+//create styled component for photo
+const StyledPhoto = styled.img`
+  width: 25%;
+  padding: 4px;
+`;
+
 const Carousel = (props) => {
+  {/* iterate over the props passed in and create a photo for each index */}
+  {for (var i = 0; i < props.photos.length; i++) {
+    //create a styled photo for each one
+    <StyledPhoto url={props.photos[i].url} />
+  }}
   return (
     <div>
       <button onClick={props.changeView}> Back </button>
       In the Carousel!
+      {/* <SaveIcon /> */}
+      <div>
+        {props.photos.map((currPhoto) => {
+          return <StyledPhoto src={currPhoto.url} alt={`styledPhoto${currPhoto}`} />
+        })}
+      </div>
     </div>
   );
 };
